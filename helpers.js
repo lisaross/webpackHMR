c26515cf673ee1f6a66d5da4ad88f145345a60db
Lisa Ross <lisa@thisisbroad.com>
2019-04-30 17:11:39 -0400

    setup includes pug templating, webpack for dev and prod, node server, routes, sass, ecmascript

---
+++ helpers.js
@@ -0,0 +1,5 @@
+// Some details about the site
+exports.siteName = 'Now That\'s Delicious!';
+
+// Dump is a handy debugging function we can use to sort of "console.log" our data
+exports.dump = (obj) => JSON.stringify(obj, null, 2);
---
+++ webpack.prod.js
@@ -0,0 +1,15 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.config.js');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = merge(common, {
+  mode: 'production',
+  devtool: 'source-map',
+  optimization: {
+    minimizer: [
+      new TerserPlugin({}),
+      new OptimizeCSSAssetsPlugin({})
+    ],
+  }
+});
